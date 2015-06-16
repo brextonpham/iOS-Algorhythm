88f267085c64707c23e85e922835dff0c5038bdc
Brexton <brextonpham@stanford.edu>
2015-06-15 20:33:32 -0400

    Uploading Algorhythm
    Playlist browser app built in Obj-C

---
+++ Algorhythm/PlaylistDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlaylistDetailViewController.m
+//  Algorhythm
+//
+//  Created by Brexton Pham on 6/15/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import "PlaylistDetailViewController.h"
+#import "Playlist.h"
+
+@interface PlaylistDetailViewController ()
+
+@end
+
+@implementation PlaylistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    if (self.playlist) {
+        self.playlistCoverImage.image = self.playlist.playlistIconLarge;
+        self.playlistCoverImage.backgroundColor = self.playlist.backgroundColor;
+        self.playlistTitle.text = self.playlist.playlistTitle;
+        self.playlistDescription.text = self.playlist.playlistDescription;
+        
+        self.playlistArtist0.text = self.playlist.playlistArtists[0];
+        self.playlistArtist1.text = self.playlist.playlistArtists[1];
+        self.playlistArtist2.text = self.playlist.playlistArtists[2];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
